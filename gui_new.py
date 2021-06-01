98d0dfc8626b070bd7b15bb815364d4c4379c0b0
ruthvikb14 <34194953+ruthvikb14@users.noreply.github.com>
2021-06-01 16:19:48 +0200

    fixed previous errors
    - added new button to update graph
    - plots starts from (0,0)
    - fixed the subprocess function to run on both windows and mac

--- gui_new.py
+++ gui_new.py
@@ -14,6 +14,12 @@ from matplotlib.figure import Figure
 import warnings
 warnings.filterwarnings('ignore')
 
+_bx = None
+_by = None
+_bz = None
+_red = None
+_green = None
+_blue = None
 
 class chef():
     __root = Tk()
@@ -90,31 +96,31 @@ class chef():
         self.toolbar.update()
         
         # Add variables
-        self.__Var1 = BooleanVar()
-        self.__Var2 = BooleanVar()
-        self.__Var3 = BooleanVar()
-        self.__Var4 = BooleanVar()
-        self.__Var5 = BooleanVar()
-        self.__Var6 = BooleanVar()
+        self.__Var1 = BooleanVar(self.__root)
+        self.__Var2 = BooleanVar(self.__root)
+        self.__Var3 = BooleanVar(self.__root)
+        self.__Var4 = BooleanVar(self.__root)
+        self.__Var5 = BooleanVar(self.__root)
+        self.__Var6 = BooleanVar(self.__root)
         
         # Add CheckButtons (widget)
         self.checkBtn()
-        self._bx = None
-        self._by = None
-        self._bz = None
-        self._red = None
-        self._green = None
-        self._blue = None
+        # self._bx = None
+        # self._by = None
+        # self._bz = None
+        # self._red = None
+        # self._green = None
+        # self._blue = None
         
         # Add Table (widget)
         self.table()
-        #self.__root.bind("<Button-1>",self.update_event)
         
         # Add Text (widget) 
         self.text()
         
         # To open a already existing file 
         self.__thisFileMenu.add_command(label="Open", command=self.__openFile)
+        self.__thisFileMenu.add_command(label="Save As", command=self.__saveFile)
         self.__thisFileMenu.add_separator()
         self.__thisFileMenu.add_command(label="Exit", command=self.__exit)
         self.__thisMenuBar.add_cascade(label="  File  ", menu=self.__thisFileMenu, underline=0)
@@ -125,6 +131,10 @@ class chef():
         
         self.__root.config(menu=self.__thisMenuBar)
         self.config()
+        
+        self.__upg = Button(self.__root, text = "Update Graph", padx=15, pady=5, 
+                    font=("Arial", 10, "bold"), bg = "black", fg = "white", command = self.__update)
+        self.__upg.grid(column=5,row=9, columnspan=2,sticky='NW')
         
         mag = Label(extra, text='MagOD Chef', font=("Arial", 3, "bold")).grid(column=8, sticky='S')
         
@@ -137,10 +147,12 @@ class chef():
         self.y5 = None
         self.y6 = None
     
+    """Function to create the seperation lines to indicate different recipes"""
     def v_lines(self):
         for i in self.y:
             self.__ax.axvline(self.x[i-1], linestyle='-.', color='silver') # vertical lines
     
+    """Function to create the graph and set axes"""   
     def graph(self):
         self.__ax.set_xlabel('Time(s)')
         self.__axe.set_ylabel('Led Intensity', color='crimson')
@@ -157,6 +169,7 @@ class chef():
         self.__axe.legend(loc='lower right')
         self.chart.draw()
     
+    """Function when the B_x checkbutton is pressed/cleared"""   
     def ch_bx(self):
         self.check()
         self.v_lines()
@@ -168,6 +181,7 @@ class chef():
             self.pt1.set_ydata([0])
             self.chart.draw()
     
+    """Function when the B_y checkbutton is pressed/cleared"""   
     def ch_by(self):
         self.check()
         self.v_lines()
@@ -179,6 +193,7 @@ class chef():
             self.pt2.set_ydata([0])
             self.chart.draw()
     
+    """Function when the B_z checkbutton is pressed/cleared"""   
     def ch_bz(self):
         self.check()
         self.v_lines()
@@ -190,6 +205,7 @@ class chef():
             self.pt3.set_ydata([0])
             self.chart.draw()
     
+    """Function when the red checkbutton is pressed/cleared"""   
     def ch_red(self):
         self.check()
         self.v_lines()
@@ -200,7 +216,8 @@ class chef():
             self.pt4.set_xdata([0])
             self.pt4.set_ydata([0])
             self.chart.draw()
-    
+
+    """Function when the green checkbutton is pressed/cleared"""    
     def ch_green(self):
         self.check()
         self.v_lines()
@@ -212,6 +229,7 @@ class chef():
             self.pt5.set_ydata([0])
             self.chart.draw()
     
+    """Function when the blue checkbutton is pressed/cleared"""
     def ch_blue(self):
         self.check()
         self.v_lines()
@@ -223,6 +241,7 @@ class chef():
             self.pt6.set_ydata([0])
             self.chart.draw()
     
+    """Function to create the axes when cleared"""
     def check(self):
         if((self.__Var1.get() or self.__Var2.get() or self.__Var3.get() or 
             self.__Var4.get() or self.__Var5.get() or self.__Var6.get()) and self.count):
@@ -234,33 +253,36 @@ class chef():
             self.__ax.set(ylim=[-3,3])
             self.__axe.set(ylim=[0,255])
     
+    """Function to create the checkbuttons and their placements"""
     def checkBtn(self):
-        self._bx = Checkbutton(self.__checkFrame1, text = "Bx", padx=5, pady=5, variable = self.__Var1, fg = "black",
+        _bx = Checkbutton(self.__checkFrame1, text = "Bx", padx=5, pady=5, variable = self.__Var1, fg = "black",
                 font=("Arial", 10, "bold"), command=self.ch_bx).grid(row=0,sticky='N')
 
-        self._by = Checkbutton(self.__checkFrame1, text = "By", padx=5, pady=5, variable = self.__Var2, fg = "tomato", 
+        _by = Checkbutton(self.__checkFrame1, text = "By", padx=5, pady=5, variable = self.__Var2, fg = "tomato", 
                 font=("Arial", 10, "bold"), command=self.ch_by).grid(row=1,sticky='N')
 
-        self._bz = Checkbutton(self.__checkFrame1, text = "Bz", padx=5, pady=5, variable = self.__Var3, fg = "saddle brown", 
+        _bz = Checkbutton(self.__checkFrame1, text = "Bz", padx=5, pady=5, variable = self.__Var3, fg = "saddle brown", 
                 font=("Arial", 10, "bold"), command=self.ch_bz).grid(row=2,sticky='N')
         
-        self._red = Checkbutton(self.__checkFrame2, text = " RED   ", padx=5, pady=5, variable = self.__Var4, fg = "red",
+        _red = Checkbutton(self.__checkFrame2, text = " RED   ", padx=5, pady=5, variable = self.__Var4, fg = "red",
                             font=("Arial", 10, "bold"), command=self.ch_red)
-        self._red.grid(column=8,row=0,sticky='N')
+        _red.grid(column=8,row=0,sticky='N')
 
-        self._green = Checkbutton(self.__checkFrame2, text = "GREEN", padx=5, variable = self.__Var5, fg = "green",
+        _green = Checkbutton(self.__checkFrame2, text = "GREEN", padx=5, variable = self.__Var5, fg = "green",
                             font=("Arial", 10, "bold"), command=self.ch_green)
-        self._green.grid(column=8,row=1,sticky='N')
+        _green.grid(column=8,row=1,sticky='N')
         
-        self._blue = Checkbutton(self.__checkFrame2, text = " BLUE ", padx=5, pady=5, variable = self.__Var6, fg = "blue",
+        _blue = Checkbutton(self.__checkFrame2, text = " BLUE ", padx=5, pady=5, variable = self.__Var6, fg = "blue",
                             font=("Arial", 10, "bold"), command=self.ch_blue)
-        self._blue.grid(column=8,row=2,sticky='N')
+        _blue.grid(column=8,row=2,sticky='N')
     
+    """Function to the create the table"""
     def table(self):
-        mod = TableModel(rows=30, columns=6)
+        mod = TableModel(rows=30, columns=6) # to create a table of 30*6
         self.__table = pt = TableCanvas(self.__tableFrame, model=mod)
         pt.show()
     
+    """Function for the text widget and the clear button"""
     def text(self):
         self.__text = Text(self.__root, height = 2, width = 70, xscrollcommand = set(), yscrollcommand = set())
         self.__text.grid(column=1,row=18,columnspan=4,sticky='NW')
@@ -269,6 +291,7 @@ class chef():
                     font=("Arial", 10, "bold"), bg = "black", fg = "white", command = self.__clr)
         self.__cls.grid(column=7,row=18, columnspan=2,sticky='NW')
     
+    """Configuration of the window for minimize/maximize function"""
     def config(self):
         self.__root.grid_columnconfigure(3,weight=1)
         self.__root.grid_columnconfigure(5,weight=1)
@@ -287,10 +310,12 @@ class chef():
     def __exit(self):
         self.__root.quit()
         self.__root.destroy()
-
+    
+    """Function for opening the csv file"""
     def __openFile(self):
         self.__file = askopenfilename(defaultextension=".csv", filetypes=[("CSV Files","*.csv"), ("All Files","*.*")])
         fname = os.path.basename(self.__file)
+        #print(self.__file)
         
         if self.__file == "": 
             # no file to open 
@@ -303,7 +328,10 @@ class chef():
             self.__axe.cla()
             self.count = True
             
-            w = subprocess.Popen(['TestRecipes','-o',fname],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+            try:
+                w = subprocess.Popen(['TestRecipes','-o',fname],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+            except AttributeError:
+                w = subprocess.Popen(['./TestRecipes -o',fname],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
             output, errors = w.communicate()
             self.__text.insert(END, output)
             
@@ -325,14 +353,53 @@ class chef():
             self.y4,self.y5,self.y6 = self.led_val(v5,v6)
             self.x = self.value_gen(v4,1000) # converting milli_s -> seconds
             
+            self.graph_init()
             self.graph()
     
+    """To start the graph from (0,0)"""
+    def graph_init(self):
+        self.y1.insert(0,0)
+        self.y2.insert(0,0)
+        self.y3.insert(0,0)
+        self.y4.insert(0,0)
+        self.y5.insert(0,0)
+        self.y6.insert(0,0)
+        self.x.insert(0,0)
+    
+    """Update the graph when table is updated"""
+    def toggle(self):
+        self.__ax.cla()
+        self.__axe.cla()
+        self.count = True
+        if(self.__Var1.get()): self.ch_bx()
+        if(self.__Var2.get()): self.ch_by()
+        if(self.__Var3.get()): self.ch_bz()
+        if(self.__Var4.get()): self.ch_red()
+        if(self.__Var5.get()): self.ch_green()
+        if(self.__Var6.get()): self.ch_blue()
+    
+    """Function to update the list when elements in table are updated"""
+    def __update(self):
+        mod = self.__table.model
+        self.y1 = list(map(float, mod.getColumnData(0,filters=[('B x','','contains','')])))
+        self.y2 = list(map(float, mod.getColumnData(1,filters=[('B y','','contains','')])))
+        self.y3 = list(map(float, mod.getColumnData(2,filters=[('B z','','contains','')])))
+        v4 = list(map(int, mod.getColumnData(3,filters=[('Led Time','','contains','')])))
+        v5 = mod.getColumnData(4,filters=[('Led Color','','contains','')])
+        v6 = list(map(int, mod.getColumnData(5,filters=[('Led Intensity','','contains','')])))
+        self.y4,self.y5,self.y6 = self.led_val(v5,v6)
+        self.x = self.value_gen(v4,1000) # converting milli_s -> seconds
+        self.toggle()
+        
+    
+    """Function to convert milli-seconds to seconds"""
     def value_gen(self,a,b):
         val = [0]*len(a)
         for i,j in enumerate(a):
             val[i]=val[i-1]+int(j/b)
         return val
     
+    """Function to clear the contents and redraw the table"""
     def clear_table(self):
         mod = self.__table.model
         r = list(i for i in range(mod.getRowCount()))
@@ -342,13 +409,16 @@ class chef():
         self.__table.clearData()
         self.__table.redraw()
     
+    """ Function to count the number of recipes. It returns the list to draw 
+    seperations on the graph"""
     def freq_count(self, vv): 
         freq = {}
         for i in vv: 
             freq[i] = vv.count(i)
         a = list(freq.values())
         return a
-
+    
+    """Function to convert the LED Color to integers"""
     def led_val(self, val, _int):
         _green=[0]*len(val)
         _red=[0]*len(val)
@@ -365,7 +435,20 @@ class chef():
                 _red[i]=0
                 _blue[i]=0
         return _red,_green,_blue
-
+    
+    """Function to save file"""
+    def __saveFile(self):
+        if self.__sfile == None:
+        # Save as new file
+            self.__sfile = asksaveasfilename(initialfile='Untitled.csv', defaultextension=".csv", filetypes=[("CSV Files","*.csv"), ("All Files","*.*")])
+        if self.__sfile == "":
+            self.__sfile = None
+        else:
+            # Try to save the file
+            self.__table.exportTable(self.__sfile)
+            # Change the window title
+            self.__root.title(os.path.basename(self.__sfile))
+    
     def execute(self): 
     # Run main application 
         self.__root.mainloop() 
